5bc5c7188ed52f2ceb449e365260f01922307974
HP <HP@192.168.0.110>
2024-02-10 00:40:19 -0500

    CatalogoOnlineCamisas

---
+++ src/main/java/com/mycompany/catalogoonline/Catalogoonline.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.catalogoonline;
+
+/**
+ *
+ * @author HP
+ */
+public class Catalogoonline {
+
+    public static void main(String[] args) {
+        GUICamisas view = new GUICamisas();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+    }
+}
